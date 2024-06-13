7b862b2f1cc55aba8540006fb860e35d65c34cd5
kengon <kengon56@gmail.com>
2024-06-13 14:59:00 +0700

    added feed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,35 @@
-import Image from "next/image";
+import Post from "@/app/components/Post";
+import prisma from "@/app/lib/prisma";
 
-export default function Home() {
+async function getPosts() {
+  const posts = await prisma.post.findMany({
+    where: { published: true },
+    include: {
+      author: {
+        select: { name: true, email: true },
+      },
+    },
+  });
+  return posts;
+}
+
+export default async function Home() {
+  const posts = await getPosts();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1>Welcome to my fisrt app</h1>
-      <p>This is my first app</p>
+      <h1>FEED</h1>
+      {posts.map((post) => {
+        return (
+          <Post
+            key={post.id}
+            id={post.id}
+            title={post.title}
+            content={post.content}
+            authorName={post.author?.name}
+            authorEmail={post.author?.email}
+          />
+        );
+      })}
     </main>
   );
 }
