9a6040feedf57e7cec0ced354bb7b3deb20a21ed
kengon <kengon56@gmail.com>
2024-06-13 10:00:16 +0700

    new

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1>Welcome to my fisrt app</h1>
+      <p>This is my first app</p>
+    </main>
+  );
+}
